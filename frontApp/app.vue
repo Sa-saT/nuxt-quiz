ff221e757f135f9de2f90b4b275e5a04a4d68ab1
Sa-saT <sasa.is.music@gmail.com>
2024-07-26 00:45:43 +0900

    add 20240726

---
+++ frontApp/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h1 class="text-3xl font-bold underline text-blue-500">
+    Hello world!
+    </h1>
+    <!-- <NuxtPage /> -->
+  </div>
+</template>
