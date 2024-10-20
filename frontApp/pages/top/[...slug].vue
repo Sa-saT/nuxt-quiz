936a91be4dc40396264fc2d471f302a415186e2a
Sa-saT <sasa.is.music@gmail.com>
2024-10-20 23:30:08 +0900

    add 241020 a

---
+++ frontApp/pages/top/[...slug].vue
@@ -0,0 +1,15 @@
+<script setup>
+    // 変数
+    const msg = 'Hello!'
+
+    console.log(msg)
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <h2>Top</h2>
+        <p>{{ $route.params.slug }}</p>
+    </div>
+</template>
